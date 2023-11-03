1f7d97857d27397bef8bcaec08ecd5b153fa5a90
2Kelvin <rocketmancodes02@gmail.com>
2023-11-04 01:06:48 +0300

    tasks 1 & 2 working properly for task 2 I added the important value so that the class could override the id and for task 1 I used this to fix the repetitive amenity id check

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,20 +1,15 @@
 $(function () {
-    const inputTag = $('input');
-    const h4 = $('.amenities').find('h4');
-    let amenityId = '';
-    let amenityName = '';
-    const amentyArray = [];
+  const inputTag = $('input:checkbox');
+  const h4 = $('.amenities h4');
+  const amentyArray = [];
 
-    inputTag.bind('change', function () {
-        let isChecked = inputTag.is(':checked');
-        if (isChecked) {
-            amenityId = inputTag.attr('data-id');
-            amenityName = inputTag.attr('data-name');
-            amentyArray.push(amenityId);
-            const allAmenties = amentyArray.join(', ');
-            h4.text(allAmenties);
-        } else {
-            amenityId = '';
-        }
-    });
+  inputTag.bind('change', function () {
+    if ($(this).is(':checked')) {
+      amentyArray.push($(this).attr('data-name'));
+    } else {
+      const aId = amentyArray.indexOf($(this).attr('data-name'));
+      amentyArray.splice(aId, 1);
+    }
+    h4.text(amentyArray.join(', '));
+  });
 });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,10 +1,25 @@
 $(function () {
-    const reqUrl = 'http://0.0.0.0:5001/api/v1/status/';
-    $.get(reqUrl, function(data, status) {
-        if (status === 'OK') {
-            $('#api_status').addClass('available');
-        } else {
-            $('#api_status').removeClass('available');
-        }
-    });
+  const inputTag = $('input:checkbox');
+  const h4 = $('.amenities h4');
+  const amentyArray = [];
+
+  inputTag.bind('change', function () {
+    if ($(this).is(':checked')) {
+      amentyArray.push($(this).attr('data-name'));
+    } else {
+      const aId = amentyArray.indexOf($(this).attr('data-name'));
+      amentyArray.splice(aId, 1);
+    }
+    h4.text(amentyArray.join(', '));
+  });
+
+  const reqUrl = 'http://0.0.0.0:5001/api/v1/status/';
+  const divCircle = $('#api_status');
+  $.get(reqUrl, function (data) {
+    if (data.status === 'OK') {
+      divCircle.addClass('available');
+    } else {
+      divCircle.removeClass('available');
+    }
+  });
 });
